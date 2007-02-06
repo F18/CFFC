21b4ccf33d1172ecec49af911e46e27cf132c2ae
lucian <lucian>
2007-02-06 19:44:24 +0000

    Fine tunne the makefile

--- src/Common/SourceRevisionData.cc
+++ src/Common/SourceRevisionData.cc
@@ -4,8 +4,8 @@
 #include "SourceRevisionData.h"
 
 char* SourceCode::CodeName="Reconstruction++";
-char* SourceCode::Revision="5:6M";
-char* SourceCode::CompilationTime="Tue 06 Feb 2007 02:02:00 PM EST";
+char* SourceCode::Revision="7M";
+char* SourceCode::CompilationTime="Tue 06 Feb 2007 02:43:18 PM EST";
 char* SourceCode::LastChanged_Author="lucian";
-char* SourceCode::LastChanged_Revision="5";
-char* SourceCode::LastChanged_Date="Sun, 04 Feb 2007";
+char* SourceCode::LastChanged_Revision="7";
+char* SourceCode::LastChanged_Date="Tue, 06 Feb 2007";
