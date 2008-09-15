4004d412f30f0e1ccb612410713afa6502e89088
Willem Deconinck <willem.deconinck@utoronto.ca>
2008-09-15 19:13:07 -0400

    fix of outputbug
    Former-commit-id: 6e3218d04eb2e894d63cd9feeef908996187b256

--- src_3D/ExplicitFilters/Explicit_Filter.h
+++ src_3D/ExplicitFilters/Explicit_Filter.h
@@ -772,7 +772,7 @@ void Explicit_Filters<Soln_pState,Soln_cState>::Calculate_Commutation_Error_Bloc
     assert (!commutation_error_norms_file.fail( )); 
     
     commutation_error_norms_file 
-    << ((Soln_Blks[nBlk].ICu-properties.number_of_rings_increased) - (Soln_Blks[nBlk].ICl+properties.number_of_rings_increased) + 1)
+    << ((Grid_Blk.ICu-properties.number_of_rings_increased) - (Grid_Blk.ICl+properties.number_of_rings_increased) + 1)
     << " " << Commutation_Error_maxnorm(0)
     << " " << Commutation_Error_L1norm(0)
     << " " << Commutation_Error_L2norm(0)
@@ -785,7 +785,7 @@ void Explicit_Filters<Soln_pState,Soln_cState>::Calculate_Commutation_Error_Bloc
     assert (!truncation_error_norms_file.fail( )); 
     
     truncation_error_norms_file 
-    << ((Soln_Blks[nBlk].ICu-properties.number_of_rings_increased) - (Soln_Blks[nBlk].ICl+properties.number_of_rings_increased) + 1)
+    << ((Grid_Blk.ICu-properties.number_of_rings_increased) - (Grid_Blk.ICl+properties.number_of_rings_increased) + 1)
     << " " << maxnorm(Grid_Blk, Truncation_Error)(0)
     << " " << p_norm(Grid_Blk, Truncation_Error, 1)(0)
     << " " << p_norm(Grid_Blk, Truncation_Error, 2)(0)
