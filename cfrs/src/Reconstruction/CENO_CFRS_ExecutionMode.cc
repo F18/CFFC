0b79ed76de3eacc95ead3f9acf9b6d9f0d63a539
Ramy Rashad <rashad@utias.utoronto.ca>
2009-03-08 13:04:11 -0400

    cfrs: add the execution mode files
    - forgot to do this on the last commit
    
    
    Former-commit-id: ced8069037355b0de66aadeb7bbb50c9dee95766

---
+++ cfrs/src/Reconstruction/CENO_CFRS_ExecutionMode.cc
@@ -0,0 +1,119 @@
+/*!\file CENO_ExecutionMode.cc
+  \brief Initialize the flags that control the execution of CENO high-order reconstruction. */
+
+/* Include required C++ libraries. */
+// None
+
+/* Using std namespace functions */
+// None
+
+/* Include CFFC header files */
+#include "../../../src_2D/CFD/CFD.h"
+#include "CENO_CFRS_ExecutionMode.h"
+
+short CENO_CFRS_Execution_Mode::USE_PSEUDO_INVERSE = OFF; // computation memory efficient mode
+//short CENO_CFRS_Execution_Mode::CENO_APPLY_GEOMETRIC_WEIGHTING = ON;
+//! Set all flags to default values
+// add all flag default values to this function
+void CENO_CFRS_Execution_Mode::SetDefaults(void){
+  
+  USE_PSEUDO_INVERSE = OFF; // computation memory efficient mode
+  // CENO_APPLY_GEOMETRIC_WEIGHTING == ON
+}
+
+//! Print the current execution mode
+//  at the output stream
+// \param [in] out_file the output stream
+void CENO_CFRS_Execution_Mode::Print_Info(std::ostream & out_file){
+
+  // output execution mode
+  if (USE_PSEUDO_INVERSE == ON){
+    out_file << "\n     -> Execution Mode: " << "Speed Efficient: Pseudo Inverse Used";
+  } else {
+    out_file << "\n     -> Execution Mode: " << "Memory Efficient: Pseudo Inverse Not Used";
+  }
+
+  // output subroutine used to solver the least-squares problem
+  if (USE_PSEUDO_INVERSE == OFF){
+    out_file << "\n     -> Least-squares solver";
+  }
+
+//  // output geom weighting type
+//  if (CENO_APPLY_GEOMETRIC_WEIGHTING == ON){
+//    if (CENO_SQUARE_GEOM_WEIGHTING == ON){
+//      out_file << "\n     -> Geom Weighting: " << "Inverse of squared distance";
+//    } else {
+//      out_file << "\n     -> Geom Weighting: " << "Inverse distance";
+//    }
+//  } else {
+//    out_file << "\n     -> Geom Weighting: " << "Turned OFF";
+//  }
+
+}
+
+/*!
+ * Broadcast the CENO_CFRS_Execution_Mode variables to all      
+ * processors associated with the specified communicator
+ * from the specified processor using the MPI broadcast 
+ * routine.
+ */
+//void CENO_CFRS_Execution_Mode::Broadcast(void){
+//#ifdef _MPI_VERSION
+//  
+//  MPI::COMM_WORLD.Bcast(&USE_CENO_ALGORITHM,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&USE_PSEUDO_INVERSE,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_DROP_ORDER,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_PADDING,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_APPLY_GEOMETRIC_WEIGHTING,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_SQUARE_GEOM_WEIGHTING,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_CONSIDER_WEIGHTS,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&FORCE_WITH_PIECEWISE_CONSTANT_AT_INTERFACE,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_RECONSTRUCTION_WITH_MESSAGE_PASSING,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_SMOOTHNESS_INDICATOR_COMPUTATION_WITH_ONLY_FIRST_NEIGHBOURS,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_CONSTRAINED_RECONSTRUCTION_WITH_ADDITIONAL_APPROXIMATE_CONSTRAINTS,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_CONSTRAINED_RECONSTRUCTION_WITH_EXTENDED_BIASED_STENCIL,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&USE_LAPACK_LEAST_SQUARES,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&IGNORE_CURVED_BOUNDARIES_FOR_ACCURACY_ASSESSMENT,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&USE_SMOOTHNESS_INDICATOR_FOR_AMR_CRITERIA,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&HIGH_ORDER_MESSAGE_PASSING,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&CENO_VERBOSE,
+// 			1, 
+// 			MPI::SHORT, 0);
+//  MPI::COMM_WORLD.Bcast(&Limiter,
+// 			1, 
+// 			MPI::SHORT, 0);  
+//
+//#endif
+//}
