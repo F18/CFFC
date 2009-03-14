2598b309a7e47b46ed333e8a52196251f720cc0f
Ramy Rashad <rashad@utias.utoronto.ca>
2009-03-14 01:13:03 -0400

    WIP: add the high-level TaylorDerivatives.h file
    Former-commit-id: 4e0c472c826f6d13864933abe9b7bd1d77565475

---
+++ src_3D/HighOrderReconstruction/TaylorDerivatives.h
@@ -0,0 +1,17 @@
+/*!\file TaylorDerivatives.h
+  \brief Header file required to be included in order to use "TaylorDerivatives" objects. */
+
+#ifndef _TAYLORDERIVATIVES_INCLUDED
+#define _TAYLORDERIVATIVES_INCLUDED
+
+/* Include required C++ libraries. */
+// None
+
+/* Using std namespace functions */
+// None
+
+/* Include CFFC header files */
+#include "Containers/TaylorDerivatives_Template.h"
+#include "Containers/TaylorDerivatives3D.h"
+
+#endif
