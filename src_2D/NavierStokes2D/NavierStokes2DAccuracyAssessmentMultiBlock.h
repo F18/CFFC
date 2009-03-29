533487297aa0a4b85c7b19db8b3e045209f3a61c
Lucian Ivan <lucian.ivan@utoronto.ca>
2009-03-29 15:33:31 -0400

    Clean up aerodynamic forces calculation.
    Former-commit-id: 5fd38e54fe0ba054ff867658d1e5b80f742c2316

--- src_2D/NavierStokes2D/NavierStokes2DAccuracyAssessmentMultiBlock.h
+++ src_2D/NavierStokes2D/NavierStokes2DAccuracyAssessmentMultiBlock.h
@@ -61,7 +61,11 @@ AssessSolutionAccuracyBasedOnLiftAndDragCoefficients(NavierStokes2D_Quad_Block *
     double alpha;			 // angle of attack
     alpha = TWO*PI*IP.Flow_Angle/360.00; // angle of attack in radians
     double cos_alpha(cos(alpha)), sin_alpha(sin(alpha)); // cosine and sine of the angle of attach
-    double Fx, Fy;
+
+    vector<double> Fx;
+    Fx.assign(NumberOfSolidBodies, 0.0); // Net force in x-direction for each body
+    vector<double> Fy;
+    Fy.assign(NumberOfSolidBodies, 0.0); // Net force in y-direction for each body
     vector<double> DummySolidBodyLength; DummySolidBodyLength.assign(NumberOfSolidBodies, 0.0);
 
     // Counters
@@ -79,42 +83,34 @@ AssessSolutionAccuracyBasedOnLiftAndDragCoefficients(NavierStokes2D_Quad_Block *
 	  if (IP.i_Reconstruction == RECONSTRUCTION_HIGH_ORDER){
 	    // Add skin friction forces only
 	    if (SolnBlk[nb].Flow_Type != FLOWTYPE_INVISCID){
-	      SolnBlk[nb].AssessAccuracy.addWallShearStressAerodynamicForcesHighOrder(Drag,Lift,SolidBodyLength,
+	      SolnBlk[nb].AssessAccuracy.addWallShearStressAerodynamicForcesHighOrder(Fx,Fy,
+										      SolidBodyLength,
 										      PointTest);
 	    }
 	  }
 	
 	} //endif
       } //endfor
-   
-      CFFC_Barrier_MPI(); // MPI barrier to ensure processor synchronization.
-
-      for (nb = 0; nb < NumberOfSolidBodies; ++nb){
-	/* Total aerodynamic force Fy on all CPUs for each of the solid bodies */
-	Drag[nb] = CFFC_Summation_MPI(Drag[nb]);
-
-	/* Total wetted surface on all CPUs for each of the solid bodies */
-	SolidBodyLength[nb] = CFFC_Summation_MPI(SolidBodyLength[nb]);
-      }
-
+      
     } else {
+
       // Compute the contributions to aerodynamic forces (i.e. Fx and Fy)
       // on each solid body due to each block on the current CPU.
-      // Lift variable stores Fx and Drag variable stores Fy
       for (nb = 0; nb < Soln_Block_List.Nblk; ++nb) {
 	if (Soln_Block_List.Block[nb].used == ADAPTIVEBLOCK2D_USED && SolnBlk[nb].Grid.IsThereAnySolidBoundary() ) {
 	
 	  if (IP.i_Reconstruction == RECONSTRUCTION_HIGH_ORDER){
 	    // Use the first high-order object to assess the accuracy
-	    SolnBlk[nb].AssessAccuracy.addAerodynamicForcesHighOrder(Lift,Drag,SolidBodyLength);
+	    SolnBlk[nb].AssessAccuracy.addAerodynamicForcesHighOrder(Fx,Fy,SolidBodyLength);
 	    // Add skin friction forces
 	    if (SolnBlk[nb].Flow_Type != FLOWTYPE_INVISCID){
-	      SolnBlk[nb].AssessAccuracy.addWallShearStressAerodynamicForcesHighOrder(Lift,Drag,DummySolidBodyLength,
+	      SolnBlk[nb].AssessAccuracy.addWallShearStressAerodynamicForcesHighOrder(Fx,Fy,
+										      DummySolidBodyLength, //< Length already calc.
 										      PointTest);
 	    }
 	  } else {
 	    // Use the low-order reconstruction
-	    SolnBlk[nb].AssessAccuracy.addAerodynamicForces(Lift,Drag,SolidBodyLength);
+	    SolnBlk[nb].AssessAccuracy.addAerodynamicForces(Fx,Fy,SolidBodyLength);
 	    // Add skin friction forces
 	    if (SolnBlk[nb].Flow_Type != FLOWTYPE_INVISCID){
 	      throw runtime_error("Skin friction calculation not added for the stand numerical scheme.");
@@ -124,31 +120,32 @@ AssessSolutionAccuracyBasedOnLiftAndDragCoefficients(NavierStokes2D_Quad_Block *
 	}	//endif
       } //endfor
    
-      CFFC_Barrier_MPI(); // MPI barrier to ensure processor synchronization.
+    } // endif
 
-      for (nb = 0; nb < NumberOfSolidBodies; ++nb){
-	/* Total aerodynamic force Fx on all CPUs for each of the solid bodies */
-	Lift[nb] = CFFC_Summation_MPI(Lift[nb]);
+    CFFC_Barrier_MPI(); // MPI barrier to ensure processor synchronization.
+
+    /* =======================================================================
+       =            Sum up all contributions from different blocks           =
+       ======================================================================= */
+
+    for (nb = 0; nb < NumberOfSolidBodies; ++nb){
+      /* Total aerodynamic force Fx on all CPUs for each of the solid bodies */
+      Fx[nb] = CFFC_Summation_MPI(Fx[nb]);
       
-	/* Total aerodynamic force Fy on all CPUs for each of the solid bodies */
-	Drag[nb] = CFFC_Summation_MPI(Drag[nb]);
+      /* Total aerodynamic force Fy on all CPUs for each of the solid bodies */
+      Fy[nb] = CFFC_Summation_MPI(Fy[nb]);
             
-	/* Total wetted surface on all CPUs for each of the solid bodies */
-	SolidBodyLength[nb] = CFFC_Summation_MPI(SolidBodyLength[nb]);
-      }
+      /* Total wetted surface on all CPUs for each of the solid bodies */
+      SolidBodyLength[nb] = CFFC_Summation_MPI(SolidBodyLength[nb]);
+    }
 
-      // Calculate final lift and drag forces for each solid body
-      for (nb = 0; nb < NumberOfSolidBodies; ++nb){
-	// Current aerodynamic forces in 'x' and 'y' directions
-	Fx = Lift[nb];
-	Fy = Drag[nb];
-      
-	// Lift force for the current solid body
-	Lift[nb] = Fy*cos_alpha - Fx*sin_alpha;
-
-	// Drag force for the current solid body
-	Drag[nb] = Fy*sin_alpha + Fx*cos_alpha;
-      }
+    // Calculate final lift and drag forces for each solid body
+    for (nb = 0; nb < NumberOfSolidBodies; ++nb){
+      // Lift force for the current solid body
+      Lift[nb] = Fy[nb]*cos_alpha - Fx[nb]*sin_alpha;
+	
+      // Drag force for the current solid body
+      Drag[nb] = Fy[nb]*sin_alpha + Fx[nb]*cos_alpha;
     }
 
     return 0;
